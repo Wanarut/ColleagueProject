8c1010ef89e97b78963589acb44288a811f58eaa
suriya khueankhan <suriya_khueankhan@outlook.com>
2019-04-02 23:50:05 +0700

    split file random

---
+++ wait_all_client.php
@@ -0,0 +1,35 @@
+<?php
+  require_once 'config.php';
+  session_start();
+  $team_size = $_SESSION['team_size'];
+  $pin = $_SESSION['pin'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title> Create Room </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="CSS.css">
+  </head>
+
+  <body>
+      <form action="gen_pin.php" method="POST" name="myFormLogin" id="myFormLogin" class="needs-validation" novalidate>
+          <br><br>
+          <center>
+            <center><pin> enter pin : <?php echo $pin ?></pin></center>
+            <div class="col-sm-6">
+              <button type="submit" class="btn join"> Start Grouping </button>
+            </div>
+          
+            <br>
+          </center>
+      </form>
+  </body>
+</html>
